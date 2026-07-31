092f8b7e34f06ba0ea5fce6db4d6bb2fd0c11c84
Belal Salah <belal.salah259@gmail.com>
2026-07-31 19:15:28 +0300

    feat: useState

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,25 @@
+'use client';
+
 import Header from "./components/Header";
 import Desc from "./components/Description";
 import styles from "./home.module.css";
 import Link from "next/link";
+import { useState } from "react";
 
 
 
-
-
- function Home() {
+function Home() {
   let hideDescription = true;
+   const [counter, setCounter] = useState<number>(0);
+   
+   const handleClick = () => {
+     setCounter(counter + 1);
+     console.log("Counter incremented to:", counter);
+   }
+
+
+
+
   return (
     <div >
       <main style={{ color: "red" , backgroundColor: "green " }}>
@@ -17,6 +28,11 @@ import Link from "next/link";
        {hideDescription && <Desc  />}
        <Link href="/about">Go To About Page</Link>
       </main>
+       <div>
+       <button onClick={handleClick}>Click to Increment</button>
+       <p>Counter: {counter}</p>
+
+       </div>
     </div>
   );
 }
